0db1d3f296cc929b49e8f3b189e272bb9f85098e
RubenAriasM <rubenarmar2005@gmail.com>
2024-10-08 11:06:44 +0200

    Hago cosas

--- app/src/main/java/com/example/tutorialapp/intents/IntentImplicito.java
+++ app/src/main/java/com/example/tutorialapp/intents/IntentImplicito.java
@@ -1,7 +1,12 @@
 package com.example.tutorialapp.intents;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -40,6 +45,28 @@ public class IntentImplicito extends AppCompatActivity {
         textViewComida.setText(comida);
         textViewBebida.setText(bebida);
         Log.d("Prueba", textoPrueba);
+
+        //Intent Implicito componente
+        ImageButton imageButton = findViewById(R.id.ImageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //INTERNET URL
+                String url="https://www.yelp.com/search?cflt=kebab&find_loc=Gij%C3%B3n%2C+Asturias";
+                Intent intentUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentUrl);
+
+                //ALARMA
+                Intent intentAlarma = new Intent(AlarmClock.ACTION_SET_ALARM)
+                        .putExtra(AlarmClock.EXTRA_MESSAGE, "message")
+                        .putExtra(AlarmClock.EXTRA_HOUR, 8)
+                        .putExtra(AlarmClock.EXTRA_MINUTES, 43);
+                startActivity(intentAlarma);
+
+
+            }
+        });
 
     }
 }
